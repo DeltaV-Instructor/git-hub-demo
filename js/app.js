f1d5b406a51dd81a4850e0fb2211724c6e92685d
tektechnologies <info@tektechnologies.com>
2023-02-28 10:24:09 -0600

    add index css and js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+
+console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');console.log('app js is connected!');
